373636cf832f48867b3dafd921420aa1eb69e624
Ryan McGrath <ryan@venodesigns.net>
2009-11-05 03:37:45 -0500

    SQLite sample data/DB, minor modifications to Slide Model, changed one or two settings

--- slides/models.py
+++ slides/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Slide(models.Model):
-	title = models.CharField(max_length=200)
-	body = models.TextField(blank=True)
+	title = models.CharField(max_length = 200)
+	body = models.TextField(blank = True)
 	status_modes = (
 		(0, 'Draft'),
 		(1, 'Published'),
 	)
-	status = models.IntegerField(choices = status_modes, default=0)
+	status = models.IntegerField(choices = status_modes, default = 0)
